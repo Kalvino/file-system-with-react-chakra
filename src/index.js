9d1adb24acaf0df5dadca17e1381fbfca43621c0
calvin <calvodira@gmail.com>
2022-01-31 23:17:15 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+   
+ ReactDOM.render(
+  <React.StrictMode>
+   <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode="light"></ColorModeScript>
+      <App />
+   </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
